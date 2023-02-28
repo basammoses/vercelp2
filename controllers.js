e213cc55112f2b7a71dc82a20dc97e6e95e1c0d4
Basam Hassan <basammoses@gmail.com>
2023-02-28 08:19:53 -0500

    update21323

--- controllers.js
+++ controllers.js
@@ -120,8 +120,8 @@ export const addInventoryItem = async (req, res) => {
   })
   try {
     await newItem.save()
-    res.status(201).json(newItem)
+    return res.status(201).json(newItem)
   } catch (error) {
-    res.status(409).json({ message: error.message })
+    return res.status(409).json({ message: error.message })
   }
 }
