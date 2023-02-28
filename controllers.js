bfe5e8b094dfd891f567e3bc455a4b40cefe8f19
Basam Hassan <basammoses@gmail.com>
2023-02-28 08:30:21 -0500

    added await functionality

--- controllers.js
+++ controllers.js
@@ -106,7 +106,7 @@ export const deleteInventoryItem = async (req, res) => {
 
 export const addInventoryItem = async (req, res) => {
   try{const {productName, companyName, year, refurbished, color, price, size, screen, stock, img} = req.body
-    const newItem =  Inventory.create({
+    const newItem =  await Inventory.create({
       productName: productName,
       companyName: companyName,
       year: year,
